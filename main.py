aec6899a2270a4493b14f1e0a0ee5ee0dda48d81
Salas-Carlos <45397749+Salas-Carlos@users.noreply.github.com>
2019-09-05 11:36:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def inicio():
 
   return render_template('index.html')
 
-
+def escribir(tiempo,temp,hum):
+  hora= time.localtime()
+  horaT= time.strftime("%H:%M:%S", hora)
+  print(horaT)
+  datos=open("09052019.csv","a+")
+  datos.write("\r\n"+horaT+", "+tiempo+", "+ temp+ ", "+ hum )
+  datos.close()
 
 @app.route('/log', methods=['GET'])
 def guardar():
@@ -17,12 +23,7 @@ def guardar():
   tiempo = request.args.get('tiempo')
   temp = request.args.get('temperatura')
   hum = request.args.get('humedad')
-  hora= time.localtime()
-  horaT= time.strftime("%H:%M:%S", hora)
-  print(horaT)
-  datos=open("09052019.csv","a+")
-  datos.write("\r\n"+horaT+", "+tiempo+", "+ temp+ ", "+ hum )
-  datos.close()
+  escribir(tiempo,temp,hum)
   return render_template('index.html')
 
 
