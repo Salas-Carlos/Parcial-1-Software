04b1ec98c3254279d1c9dd8f9e491c5d5b8a87be
Salas-Carlos <45397749+Salas-Carlos@users.noreply.github.com>
2019-09-05 11:04:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import datetime
+import time
+app=Flask(__name__)
+
+@app.route('/')
+def inicio():
+
+
+  return render_template('index.html')
+
+
+
+@app.route('/log', methods=['GET'])
+def guardar():
+
+  tiempo = request.args.get('tiempo')
+  temp = request.args.get('temperatura')
+  hum = request.args.get('humedad')
+  hora= time.localtime()
+  horaT= time.strftime("%H:%M:%S", hora)
+  print(horaT)
+  datos=open("09052019.csv","a+")
+  datos.write("\r\n"+horaT+", "+tiempo+", "+ temp+ ", "+ hum )
+  datos.close()
+  return render_template('index.html')
+
+
+
+
+
+
+app.run(debug=True)
